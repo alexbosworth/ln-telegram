de9c954d13021b8af42f08629938e8ba975f2d18
Bota <Bota@ASUS>
2022-03-26 16:17:57 +0100

    show millisats in the forwarded message if "full preferred tokens type" is set

--- post/consolidate_forwards.js
+++ post/consolidate_forwards.js
@@ -28,10 +28,10 @@ module.exports = ({forwards}) => {
 
     sum[pair] = sum[pair] || {};
 
-    sum[pair].fee = (sum[pair].fee || Number()) + forward.fee;
+    sum[pair].fee = (sum[pair].fee || Number()) + Number(forward.fee_mtokens) / 1e3;
     sum[pair].incoming_channel = forward.incoming_channel;
     sum[pair].outgoing_channel = forward.outgoing_channel;
-    sum[pair].tokens = (sum[pair].tokens || Number()) + forward.tokens;
+    sum[pair].tokens = (sum[pair].tokens || Number()) + Number(forward.mtokens) / 1e3;
 
     return sum;
   },
