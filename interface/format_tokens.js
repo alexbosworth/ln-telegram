5df9bd68b57967f94d91b7e44164cb064280b79e
Alex Bosworth <alex.bosworth@gmail.com>
2023-11-02 08:33:15 -0700

    merge changes

--- interface/format_tokens.js
+++ interface/format_tokens.js
@@ -1,6 +1,7 @@
 const fullTokensType = 'full';
-const roundedTokensType = 'rounded';
 const isString = n => typeof n === 'string';
+const {round} = Math;
+const roundedTokensType = 'rounded';
 const tokensAsBigUnit = tokens => (tokens / 1e8).toFixed(8);
 
 /** Format tokens for display
@@ -23,8 +24,11 @@ module.exports = ({none, tokens}) => {
   // Exit early for tokens environment displays the value with no leading zero
   if (process.env.PREFERRED_TOKENS_TYPE === fullTokensType) {
     return {display: tokens.toLocaleString()};
-  } else if (process.env.PREFERRED_TOKENS_TYPE === roundedTokensType) {
-    return {display: Math.round(tokens).toLocaleString()};
+  }
+
+  // Exit early for tokens environment displaying a rounded, non-leading zero
+  if (process.env.PREFERRED_TOKENS_TYPE === roundedTokensType) {
+    return {display: round(tokens).toLocaleString()};
   }
 
   return {display: tokensAsBigUnit(tokens)};
